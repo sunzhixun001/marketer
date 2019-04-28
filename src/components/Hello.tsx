c2a0e3ab46d1f3aad1fa755c2cbf329aaa43f385
crx <crx@rongxiang.local>
2019-04-28 17:58:24 +0800

    04-28

---
+++ src/components/Hello.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default class Hello extends React.Component {
+    render() {
+        return (
+            <div>fuck you</div>
+        )
+    }
+}
---
+++ src/routers.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Router, Switch  } from 'react-router';
+
+export default (
+    <Router>
+        <Route path="/">
+            <Route path="/demo">
+            </Route>
+        </Route>
+    </Router>
+  
+);
