9d7a76ff19fc48ae5dd13a40a68e70dbe045a691
sun <sun@sundeMacBook-Pro.local>
2019-04-30 00:21:58 +0800

    0430-1

--- src/components/User/Form.js
+++ src/components/User/Form.js
@@ -1,11 +1,12 @@
 import React from 'react';
 class UserProp extends React.Component {
     render() {
-        const {title} = this.props;
+        const {title, value, icon} = this.props;
         return (
-            <div>
-                <div></div>
-                <div>{title}</div>
+            <div class='user-prop'>
+                <div class='icon'><img src={'assets/' + icon + '.png'}></img></div>
+                <div class='title'>{title}: </div>
+                <div>{value}</div>
             </div>
         )
     }
@@ -13,13 +14,18 @@ class UserProp extends React.Component {
 export default function User() {
     return (
         <div>
-            <div className='card'>
-                <UserProp title='编号'></UserProp>
-                <UserProp title='姓名'></UserProp>
-                <UserProp title='手机'></UserProp>
-                <UserProp title='公司'></UserProp>
-                <UserProp title='职位'></UserProp>
-                <UserProp title='地址'></UserProp>
+            <div className='contain'>
+                <div className='card'>
+                    <div className='logo'><img src='assets/logo.png'></img></div>
+                    <div className='user'>
+                        <UserProp title='编号' value='000001' icon='no'></UserProp>
+                        <UserProp title='姓名' value='顾晓晖' icon='name'></UserProp>
+                        <UserProp title='手机' value='13616601797' icon='mobile'></UserProp>
+                        <UserProp title='公司' value='000001' icon='company'></UserProp>
+                        <UserProp title='职位' value='000001' icon='job'></UserProp>
+                        <UserProp title='地址' value='000001' icon='address'></UserProp>
+                    </div>
+                </div>
             </div>
         </div>
     )
