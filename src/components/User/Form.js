e522a2c826aacabaac328711723ae9529376c03a
sunzhixun <sun.zhixun@tiaotiao5.com>
2019-04-30 12:29:42 +0800

    0430 2

--- src/components/User/Form.js
+++ src/components/User/Form.js
@@ -1,33 +1,65 @@
 import React from 'react';
+import axios from 'axios';
+
 class UserProp extends React.Component {
     render() {
         const {title, value, icon} = this.props;
         return (
-            <div class='user-prop'>
-                <div class='icon'><img src={'assets/' + icon + '.png'}></img></div>
-                <div class='title'>{title}: </div>
-                <div>{value}</div>
+            <div className='user-prop'>
+                <div className='left'>
+                    <div className='icon'><img src={'assets/' + icon + '.png'}></img></div>
+                    <div className='title'>{title}： </div>
+                </div>
+                <div className='value'>{value}</div>
             </div>
         )
     }
 }
-export default function User() {
-    return (
-        <div>
-            <div className='contain'>
-                <div className='card'>
-                    <div className='logo'><img src='assets/logo.png'></img></div>
-                    <div className='user'>
-                        <UserProp title='编号' value='000001' icon='no'></UserProp>
-                        <UserProp title='姓名' value='顾晓晖' icon='name'></UserProp>
-                        <UserProp title='手机' value='13616601797' icon='mobile'></UserProp>
-                        <UserProp title='公司' value='000001' icon='company'></UserProp>
-                        <UserProp title='职位' value='000001' icon='job'></UserProp>
-                        <UserProp title='地址' value='000001' icon='address'></UserProp>
+export default class User extends React.Component {
+    constructor(props){
+        super(props);
+        const {id} = props.match.params;
+        this.state = {
+            id,
+            userNo: '000000',
+            realName: '',
+            mobile: '',
+            company: '',
+            position: '',
+            address: ''
+        }
+    }
+    render() {
+        return (
+            <div className='box'>
+                <div className='contain'>
+                    <div className='card'>
+                        <div className='logo'><img src='assets/logo.png'></img></div>
+                        <div className='user'>
+                            <UserProp title='编号' value={this.state.userNo} icon='no'></UserProp>
+                            <UserProp title='姓名' value={this.state.realName} icon='name'></UserProp>
+                            <UserProp title='手机' value={this.state.mobile} icon='mobile'></UserProp>
+                            <UserProp title='公司' value={this.state.company} icon='company'></UserProp>
+                            <UserProp title='职位' value={this.state.position} icon='job'></UserProp>
+                            <UserProp title='地址' value={this.state.address} icon='address'></UserProp>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
+    componentDidMount() {
+        this.getUser({id: this.state.id, success: data => {
+            this.setState(data);
+        }});
+    }
+    getUser = ({id, success}) => {
+        fetch(`/user/${id}`)
+        .then(r => r.json())
+        .then(response => {
+            success && success(response.data);
+        });
+        // axios.get('/api/user/2');
+    }
 }
 require('./Form.css');
